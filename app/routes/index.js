f58ca06a87a4fe205f00dc309fcde185476b58d9
robdel12 <robertdeluca19@gmail.com>
2017-01-05 22:54:26 -0600

    Add title to page

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import fetch from "ember-network/fetch";
 
 export default Ember.Route.extend({
+  title: "Ember ATX Meetup",
   model() {
     return fetch('https://shielded-bayou-35314.herokuapp.com/').then(response => response.json());
   }
